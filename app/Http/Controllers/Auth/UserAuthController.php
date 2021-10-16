59bb277d240ab8fbe65d1d71f40d49243d52bcaa
Lisa9740 <barretalison@gmail.com>
2021-10-16 20:16:26 +0400

    Init project with passport authentification

---
+++ app/Http/Controllers/Auth/UserAuthController.php
@@ -0,0 +1,48 @@
+<?php
+
+namespace App\Http\Controllers\Auth;
+
+use App\Http\Controllers\Controller;
+use App\Models\User;
+use Illuminate\Http\Request;
+
+class UserAuthController extends Controller
+{
+    /**
+     * Registration
+     */
+    public function register(Request $request): \Illuminate\Http\JsonResponse
+    {
+        $this->validate($request, [
+            'name' => 'required|min:4',
+            'email' => 'required|email',
+            'password' => 'required|min:8',
+        ]);
+
+        $user = User::create([
+            'name' => $request->name,
+            'email' => $request->email,
+            'password' => bcrypt($request->password)
+        ]);
+
+        $token = $user->createToken('LaravelAuthApp')->accessToken;
+
+        return response()->json(['token' => $token], 200);
+    }
+
+    public function login(Request $request): \Illuminate\Http\JsonResponse
+    {
+        $data = [
+            'email' => $request->email,
+            'password' => $request->password
+        ];
+
+        if (auth()->attempt($data)) {
+            $token = auth()->user()->createToken('LaravelAuthApp')->accessToken;
+            return response()->json(['token' => $token], 200);
+        } else {
+            return response()->json(['error' => 'Unauthorised'], 401);
+        }
+
+    }
+}
