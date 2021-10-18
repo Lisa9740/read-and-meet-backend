ede206c4b914977dc81d08795979f103bc20d88f
Alison Barret <alison@seyes.re>
2021-10-18 16:25:29 +0400

    Added UserSeeders

---
+++ database/seeders/UserSeeder.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\Hash;
+use App\Models\User;
+
+class UserSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        $faker = \Faker\Factory::create("fr_FR");
+
+
+        for ($i = 1; $i < 4; $i++) {
+            $user = new User();
+            $user->name         = $faker->firstName();
+            $user->email        = $faker->email();
+            $user->password     = Hash::make('password');
+            $user->save();
+        }
+
+        \App\Models\User::factory(4)->create();
+
+
+    }
+}
