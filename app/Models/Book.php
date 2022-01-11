c0ac55c2bc43c402392d1e7c4d0c452dea903b9c
Lisa9740 <barretalison@gmail.com>
2022-01-11 21:40:23 +0400

    test

--- app/Models/Book.php
+++ app/Models/Book.php
@@ -10,7 +10,7 @@ class Book extends Model
     use HasFactory;
 
     protected $table = 'books';
-    protected $fillable = ['title', 'isbn_number', 'author', 'short_description'];
+    protected $fillable = ['title', 'isbn_number', 'author', 'image_thumbail_url', 'short_description'];
 
 
 
